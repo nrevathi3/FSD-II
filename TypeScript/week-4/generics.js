c05b60620f94e23a1140224cf84f7e1fa9719bef
Revathi <nrevathi0306@gmail.com>
2026-08-15 22:38:51 +0530

    Removed generated js files

--- TypeScript/week-4/generics.js
+++
@@ -1,7 +0,0 @@
-function display(value) {
-    return value;
-}
-console.log(display("TypeScript"));
-console.log(display(250));
-console.log(display(true));
-export {};
--- TypeScript/week-4/modules.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// Importing functions from math module
-Object.defineProperty(exports, "__esModule", { value: true });
-const math_1 = require("./math");
-let sum = (0, math_1.add)(15, 25);
-let product = (0, math_1.multiply)(6, 5);
-console.log("Sum =", sum);
-console.log("Product =", product);
